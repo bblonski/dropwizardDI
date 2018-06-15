18daf96d11f18c9cafe66691940718d0815e1210
Brian Blonski <brian.blonski@sri.com>
2018-06-14 17:52:07 -0700

    Priority needs to be set on interceptors in owb

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ import org.gradle.internal.deployment.RunApplication
 
 val dropwizardVersion = "1.3.2"
 val weldVersion = "3.0.4.Final"
-//val weldVersion = "2.4.7.Final"
 val jerseyVersion = "2.14"
 val owb = "2.0.6"
 plugins {
@@ -24,7 +23,6 @@ dependencies {
     compile("io.dropwizard:dropwizard-core:$dropwizardVersion")
 //    compile("org.jboss.weld.se:weld-se-shaded:$weldVersion")
     compile("org.apache.openwebbeans:openwebbeans-se:$owb")
-//    compile("org.jboss.weld.se:weld-se:$weldVersion")
     implementation("javax.enterprise:cdi-api:2.0")
     compile("org.glassfish.jersey.containers.glassfish:jersey-gf-cdi:$jerseyVersion")
     compile("javax.transaction:javax.transaction-api:1.2")
--- src/main/java/com/bblonski/dropwizard/ext/MyInterceptor.java
+++ src/main/java/com/bblonski/dropwizard/ext/MyInterceptor.java
@@ -1,11 +1,13 @@
 package com.bblonski.dropwizard.ext;
 
+import javax.annotation.Priority;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 
 @Interceptor
 @TestIntercept
+@Priority(1)
 public class MyInterceptor {
 
     @AroundInvoke
